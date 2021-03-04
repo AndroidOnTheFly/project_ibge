1f5802a7fe1c515446b00dc30556b16aae51b087
LucasMatheus99 <lucas.lacerda@academico.ifpb.edu.br>
2021-03-04 14:40:47 -0300

    Add requests in function.js

--- js/function.js
+++ js/function.js
@@ -5,6 +5,8 @@ searchButton.addEventListener('click', function () {
     try {
 
         const cityName = document.getElementById('icon_prefix').value;
+        const path = new Request(`http://localhost:3000/getSvg/${cityName}`);
+        const viewBox = new Request(`http://localhost:3000/getViewBox/${cityName}`);
 
     }
     catch(error){
