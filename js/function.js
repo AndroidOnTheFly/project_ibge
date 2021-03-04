7c85320b1ef3ece1ecb318596d1eaf5ad57e766b
LucasMatheus99 <lucas.lacerda@academico.ifpb.edu.br>
2021-03-04 10:37:41 -0300

    Remove autocomplete e Add function.js

---
+++ js/function.js
@@ -0,0 +1,13 @@
+var searchButton = document.getElementById("button-search");
+
+searchButton.addEventListener('click', function () {
+
+    try {
+
+        const cityName = document.getElementById('icon_prefix').value;
+
+    }
+    catch(error){
+        console.log("Error")
+    }
+});
